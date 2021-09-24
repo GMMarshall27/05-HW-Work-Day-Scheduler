ae348e740d62c1cd349e238d660ae8d6b21c7fe8
GMMarshall <gmmarshall27@gmail.com>
2021-09-24 14:52:13 -0400

    added function to change text area colors based on time

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
 
+var currentHour = moment().format('H');
+var input = $('textarea');
+var button = $('.saveBtn');
+
+input.each(function(i){
+    if(currentHour > i+8){$(this).addClass('past')}
+    else if(currentHour == i+8){$(this).addClass('present')}
+    else if(currentHour < i+8){$(this).addClass('future')}
+});
